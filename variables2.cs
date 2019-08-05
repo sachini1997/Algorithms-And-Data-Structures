1955ac8937cad22a4a95bc696c053d9ed87b4039
Sachini Senanayake <48913793+sachini1997@users.noreply.github.com>
2019-08-05 22:46:21 +0530

    variables2
    variables2

---
+++ variables2.cs
@@ -0,0 +1,12 @@
+int myAge = 21; //int is data type  , in this statement actually done by declarinf variable
+
+myAge = 21;  //we can say assigning variable
+
+
+//kinds of variables 
+
+int x = 46;
+double pi = 3.14;
+char y = 'N';
+bool correct = true;
+string firstName = "oshe";
